38cf5665aac6331548de1d596b04cdd9954d63bc
Trivendra <ladertrivendra@outlook.com>
2026-07-22 14:57:40 +0300

    update: RNCameraKitModule.mm

--- ios/ReactNativeCameraKit/RNCameraKitModule.mm
+++ ios/ReactNativeCameraKit/RNCameraKitModule.mm
@@ -58,6 +58,49 @@ RCT_EXPORT_METHOD(capture:(NSDictionary *)options tag:(nonnull NSNumber *)tag re
         [CKCameraManager requestDeviceCameraAuthorization:resolve reject:reject];
 }
 
+RCT_EXPORT_METHOD(scanFromUri:(NSString *)uri options:(NSDictionary *)options resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
+    NSURL *imageURL;
+    if ([uri hasPrefix:@"file://"]) {
+        imageURL = [NSURL URLWithString:uri];
+    } else {
+        imageURL = [NSURL fileURLWithPath:uri];
+    }
+
+    if (!imageURL || ![[NSFileManager defaultManager] fileExistsAtPath:imageURL.path]) {
+        reject(@"E_INVALID_URI", @"Invalid or non-existent image URI", nil);
+        return;
+    }
+
+    NSArray<NSString *> *allowedTypes = options[@"allowedBarcodeTypes"];
+
+    [ImageScanner scanBarcodesFrom:imageURL allowedBarcodeTypes:allowedTypes completion:^(NSArray<NSDictionary *> *results) {
+        resolve(results);
+    }];
+}
+
+RCT_EXPORT_METHOD(pickAndScan:(NSDictionary *)options resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+        if (!rootViewController) {
+            reject(@"E_NO_VIEW_CONTROLLER", @"No root view controller found", nil);
+            return;
+        }
+
+        NSArray<NSString *> *allowedTypes = options[@"allowedBarcodeTypes"];
+
+        [ImagePicker pickImageFrom:rootViewController completion:^(NSURL *imageURL) {
+            if (!imageURL) {
+                reject(@"E_PICKER_CANCELLED", @"Image picker was cancelled", nil);
+                return;
+            }
+
+            [ImageScanner scanBarcodesFrom:imageURL allowedBarcodeTypes:allowedTypes completion:^(NSArray<NSDictionary *> *results) {
+                resolve(results);
+            }];
+        }];
+    });
+}
+
  // Thanks to this guard, we won't compile this code when we build for the old architecture.
 #ifdef RCT_NEW_ARCH_ENABLED
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
